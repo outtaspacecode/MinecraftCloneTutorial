cd6a8a818f285866769e41a7763f77b2b77981ad
colebeebe <cole.beebe@protonmail.com>
2025-01-01 11:09:56 -0500

    Literally pulling out my hair about gitignore

--- MinecraftCloneTutorial/obj/Debug/net8.0/MinecraftCloneTutorial.AssemblyInfo.cs
+++ MinecraftCloneTutorial/obj/Debug/net8.0/MinecraftCloneTutorial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinecraftCloneTutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266beb4fd6848eeea88246d5a968dd666c48b579")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbe667b73255f0ead7921db6f25561c5572515be")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinecraftCloneTutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinecraftCloneTutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
